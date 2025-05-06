c832cf4fb6a0d478b867c37fefe539f97544e6dd
Samuel Banya <sbanya@fastmail.com>
2025-05-05 23:59:33 -0500

    Adding latest progress

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username": "Sam"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was pretty mid"},
+        {
+            "author": {"username": "BonzaiBuddy"},
+            "body": "The purple monkey was cute until it gave my Windows XP machine a virus!",
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
